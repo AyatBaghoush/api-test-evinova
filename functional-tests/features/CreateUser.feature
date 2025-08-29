285511135c1448a7229a41fc498eaf3a59265d30
¨ayatahmedelsayedmohammed.baghoush¨ <ayatahmedelsayedmohammed.baghoush@vodafone.com>
2025-08-29 03:27:49 +0200

    Adding positive and negative scenarios for create user feature

---
+++ functional-tests/features/CreateUser.feature
@@ -0,0 +1,22 @@
+Feature: Create New User
+
+    As a user of the application
+    I want to be able to create a new user
+    So that I can add them to the application
+
+@CreateUserPositive
+Scenario: Creating a new user
+  Given The "Create User" service is available
+  And   I set payload to have valid user data
+  When  I send request to create a new user
+  Then  I should receive a response code 200
+  And   Response should match JSON schema "UserCreateSchema.js"
+
+
+@CreateUserNegative
+Scenario: Creating a new user with missing password
+  Given The "Create User" service is available
+  And   I set payload to have Missing Password user data
+  When  I send request to create a new user
+  Then  I should receive a response code 400
+  And   Response should contain error message "Missing password"
---
+++ functional-tests/step-definitions/create-user-steps.js
@@ -0,0 +1,33 @@
+const { Given, When, Then, setDefaultTimeout } = require('@cucumber/cucumber');
+const chai = require('chai');
+const expect = chai.expect;
+const getLogger = require('../../src/helpers/logger');
+const logger = getLogger('fetch-users-steps');
+const HttpClient = require('../../src/infra/httpClient');
+const testDataManager = require('../../src/helpers/testDataManager');
+
+// Set default timeout for all steps (e.g., 10 seconds)
+setDefaultTimeout(10 * 1000);
+
+const httpClient = new HttpClient();
+
+When(/I set payload to have valid user data$/, async function () {
+    this.payload = await testDataManager.getValidUser(4);
+    logger.debug("The payload for creating user is: " + JSON.stringify(this.payload));
+});
+
+When(/I set payload to have Missing Password user data$/, async function () {
+    this.payload = await testDataManager.getMissingPasswordUser(4);
+    logger.debug("The payload for creating user is: " + JSON.stringify(this.payload));
+});
+
+When(/I send request to create a new user$/, async function () {
+    this.response = await httpClient.post(this.endpoint, this.payload);
+    logger.debug("The response after creating user is: " + JSON.stringify(this.response.body));
+});
+
+Then(/Response should contain error message "([^"]+)"$/, async function (expectedErrorMsg) {
+    logger.debug("The response contains error message as: " + JSON.stringify(this.response.body.error));
+    expect(this.response.body).to.have.property('error').that.includes(expectedErrorMsg);
+
+});
